892248bdbe7215f1c35ce1d46003107efacff6bd
Oksana Molchanova <mksenia728@gmail.com>
2024-11-11 13:45:09 +0100

    hw4

--- js/task-3.js
+++ js/task-3.js
@@ -1,17 +1,26 @@
 console.log('Task - 3');
 
-function filterArray(numbers, value) {
-  const resArr = [];
-  for (const elem of numbers) {
-    if (elem > value) {
-      resArr.push(elem);
-    }
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
+  
+  changeUsername(newName) {
+    this.username = newName;
+  },
+  
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
+  
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
   }
-  return resArr;
-}
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
